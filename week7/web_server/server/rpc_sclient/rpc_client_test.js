10eae538478f07207c7a05d5945476cf632981fc
congyimin <cym7079306@icloud.com>
2018-03-13 10:38:56 -0400

    tap-news

--- week7/web_server/server/rpc_sclient/rpc_client_test.js
+++
@@ -1,8 +0,0 @@
-var client = require('./rpc_client');
-
-// mongoexport --db news --collection news --out a.json
-// mongoinmport --db --test --collection test --file /Users/congyimin/Desktop/a.js
-// invoke 'add'
-client.add(1, 2, function(response) {
-  console.assert(response == 3);
-});
